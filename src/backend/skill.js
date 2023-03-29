ea5f604d2c4a7ac126ab24529bffe465b63264b9
rogersovich <dimasroger89@gmail.com>
2023-03-29 20:03:33 +0700

    update skill biografi

--- src/backend/skill.js
+++ src/backend/skill.js
@@ -5,49 +5,49 @@ const SkillAPI = [
   {
     name: "Laravel",
     level: 4,
-    desc: 'Disini isi tentang perjalanan laravel kita dan bagaimana saya bisa laravel',
+    desc: 'Menguasai dasar-dasar Laravel seperti routing, controller, model, view, dan migrasi database. \n Berpengalaman dalam penggunaan bahasa pemrograman seperti PHP, HTML, dan CSS.',
     image: Bunga
   },
   {
     name: "React JS",
     level: 5,
-    desc: 'Disini isi tentang perjalanan laravel kita dan bagaimana saya bisa laravel',
+    desc: 'Menguasai React JS dan ekosistemnya seperti Redux, React Router, dan lain lain. \n Pemahaman yang baik tentang HTML, CSS, dan JavaScript. Mampu menggunakan alat pengujian dan debugging seperti Chrome DevTools',
     image: Kodok
   },
   {
     name: "Next JS",
     level: 3,
-    desc: 'Disini isi tentang perjalanan laravel kita dan bagaimana saya bisa laravel',
+    desc: 'Menguasai dasar-dasar Next.js seperti routing, SSR (server-side rendering), dan API routing. \n Memiliki kemampuan untuk membangun aplikasi web yang responsif dan mobile-friendly menggunakan Next.js.',
     image: Kodok
   },
   {
     name: "Vue JS",
     level: 6,
-    desc: 'Disini isi tentang perjalanan laravel kita dan bagaimana saya bisa laravel',
+    desc: 'Memiliki pemahaman yang kuat tentang Vue.js, termasuk konsep-konsep dasar, pembuatan komponen, manajemen state, routing, dan lainnya. \n Mampu menggunakan Vue.js untuk mengembangkan aplikasi web yang kompleks dengan fitur seperti integrasi API, autentikasi pengguna, penggunaan library eksternal, dan sebagainya.',
     image: Bunga
   },
   {
     name: "Nuxt JS",
     level: 4,
-    desc: 'Disini isi tentang perjalanan laravel kita dan bagaimana saya bisa laravel',
+    desc: 'Menguasai dasar-dasar Nuxt.js seperti routing, SSR (server-side rendering), dan middleware. \n Memiliki kemampuan untuk membangun aplikasi web yang responsif dan mobile-friendly menggunakan Nuxt.js.',
     image: Bunga
   },
   {
     name: "Figma",
     level: 5,
-    desc: 'Disini isi tentang perjalanan laravel kita dan bagaimana saya bisa laravel',
+    desc: 'menguasai Figma basic dan mempunyai kemampuan untuk menciptakan desain dengan tampilan yang menarik dan fungsional. \n Memahami UX dan UI design untuk menghasilkan desain yang mudah digunakan dan intuitif',
     image: Kodok
   },
   {
     name: "Photoshop",
     level: 4,
-    desc: 'Disini isi tentang perjalanan laravel kita dan bagaimana saya bisa laravel',
+    desc: 'Memiliki kemampuan dalam mengedit gambar dan membuat desain sederhana dengan menggunakan software Photoshop.',
     image: Kodok
   },
   {
     name: "Mysql",
-    level: 4,
-    desc: 'Disini isi tentang perjalanan laravel kita dan bagaimana saya bisa laravel',
+    level: 3,
+    desc: 'Memiliki kemampuan dalam mengelola dan memanipulasi database MySQL serta mampu membuat query sederhana.',
     image: Bunga
   },
 ]
